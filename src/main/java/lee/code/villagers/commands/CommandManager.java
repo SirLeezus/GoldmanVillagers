376fee098f68f497607af7f6adf1c46739d05063
Lee <leecook808@gmail.com>
2023-08-29 18:44:14 -0700

    Async console

--- src/main/java/lee/code/villagers/commands/CommandManager.java
+++ src/main/java/lee/code/villagers/commands/CommandManager.java
@@ -4,11 +4,8 @@ import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import lee.code.villagers.Villagers;
 import lee.code.villagers.commands.cmds.*;
 import lee.code.villagers.lang.Lang;
-import lee.code.villagers.utils.CoreUtil;
 import lombok.Getter;
 import lombok.NonNull;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -60,8 +57,8 @@ public class CommandManager implements CommandExecutor {
 
   @Override
   public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, String[] args) {
-    if (sender instanceof Player player) {
-      if (args.length > 0) {
+    if (args.length > 0) {
+      if (sender instanceof Player player) {
         if (subCommands.containsKey(args[0].toLowerCase())) {
           final SubCommand subCommand = getSubCommand(args[0].toLowerCase());
           if (!player.hasPermission(subCommand.getPermission())) {
@@ -72,16 +69,16 @@ public class CommandManager implements CommandExecutor {
           else subCommand.perform(player, args);
           return true;
         }
-      }
-    } else if (args.length > 0) {
-      if (subCommands.containsKey(args[0].toLowerCase())) {
-        final SubCommand subCommand = getSubCommand(args[0].toLowerCase());
-        if (subCommand.performAsync()) performAsync(sender, subCommand, args);
-        else subCommand.performConsole(sender, args);
-        return true;
+      } else {
+        if (subCommands.containsKey(args[0].toLowerCase())) {
+          final SubCommand subCommand = getSubCommand(args[0].toLowerCase());
+          if (subCommand.performAsync()) performAsync(sender, subCommand, args);
+          else subCommand.performConsole(sender, args);
+          return true;
+        }
       }
     }
-    sendHelpMessage(sender);
+    getSubCommand("help").performSender(sender, args);
     return true;
   }
 
@@ -94,17 +91,27 @@ public class CommandManager implements CommandExecutor {
       }
       if (subCommand.performAsyncSynchronized()) {
         synchronized (synchronizedThreadLock) {
-          performSubCommandAsync(player, uuid, subCommand, args);
+          performPlayerSubCommandAsync(player, uuid, subCommand, args);
         }
       } else {
-        performSubCommandAsync(player, uuid, subCommand, args);
+        performPlayerSubCommandAsync(player, uuid, subCommand, args);
       }
     } else if (sender instanceof ConsoleCommandSender console) {
-      Bukkit.getAsyncScheduler().runNow(villagers, scheduledTask -> subCommand.performConsole(console, args));
+      if (subCommand.performAsync()) {
+        if (subCommand.performAsyncSynchronized()) {
+          synchronized (synchronizedThreadLock) {
+            performConsoleSubCommandAsync(sender, args, subCommand);
+          }
+        } else {
+          performConsoleSubCommandAsync(sender, args, subCommand);
+        }
+      } else {
+        subCommand.performConsole(console, args);
+      }
     }
   }
 
-  private void performSubCommandAsync(Player player, UUID uuid, SubCommand subCommand, String[] args) {
+  private void performPlayerSubCommandAsync(Player player, UUID uuid, SubCommand subCommand, String[] args) {
     asyncTasks.put(uuid, Bukkit.getAsyncScheduler().runNow(villagers, scheduledTask -> {
       try {
         subCommand.perform(player, args);
@@ -114,28 +121,7 @@ public class CommandManager implements CommandExecutor {
     }));
   }
 
-  public void sendHelpMessage(CommandSender sender) {
-    int number = 1;
-    final Map<SubCommand, String> commands = new HashMap<>();
-    for (SubCommand subCommand : getSubCommandList()) commands.put(subCommand, subCommand.getName());
-    final Map<SubCommand, String> sortedCommands = CoreUtil.sortByValue(commands, Comparator.naturalOrder());
-    final List<Component> lines = new ArrayList<>();
-    lines.add(Lang.COMMAND_HELP_DIVIDER.getComponent(null));
-    lines.add(Lang.COMMAND_HELP_TITLE.getComponent(null));
-    lines.add(Component.text(""));
-
-    for (SubCommand subCommand : sortedCommands.keySet()) {
-      if (sender.hasPermission(subCommand.getPermission())) {
-        final Component helpSubCommand = Lang.COMMAND_HELP_SUB_COMMAND.getComponent(new String[] { String.valueOf(number), subCommand.getSyntax() })
-          .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.SUGGEST_COMMAND, CoreUtil.getTextBeforeCharacter(subCommand.getSyntax(), '&')))
-          .hoverEvent(Lang.COMMAND_HELP_SUB_COMMAND_HOVER.getComponent(new String[] { subCommand.getDescription() }));
-        lines.add(helpSubCommand);
-        number++;
-      }
-    }
-
-    lines.add(Component.text(""));
-    lines.add(Lang.COMMAND_HELP_DIVIDER.getComponent(null));
-    for (Component line : lines) sender.sendMessage(line);
+  private void performConsoleSubCommandAsync(CommandSender sender, String[] args, SubCommand subCommand) {
+    Bukkit.getAsyncScheduler().runNow(villagers, scheduledTask -> subCommand.performConsole(sender, args));
   }
 }
--- src/main/java/lee/code/villagers/commands/cmds/HelpCMD.java
+++ src/main/java/lee/code/villagers/commands/cmds/HelpCMD.java
@@ -2,11 +2,14 @@ package lee.code.villagers.commands.cmds;
 
 import lee.code.villagers.Villagers;
 import lee.code.villagers.commands.SubCommand;
+import lee.code.villagers.lang.Lang;
+import lee.code.villagers.utils.CoreUtil;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class HelpCMD extends SubCommand {
 
@@ -58,7 +61,28 @@ public class HelpCMD extends SubCommand {
 
   @Override
   public void performSender(CommandSender sender, String[] args) {
-    villagers.getCommandManager().sendHelpMessage(sender);
+    int number = 1;
+    final Map<SubCommand, String> commands = new HashMap<>();
+    for (SubCommand subCommand : villagers.getCommandManager().getSubCommandList()) commands.put(subCommand, subCommand.getName());
+    final Map<SubCommand, String> sortedCommands = CoreUtil.sortByValue(commands, Comparator.naturalOrder());
+    final List<Component> lines = new ArrayList<>();
+    lines.add(Lang.COMMAND_HELP_DIVIDER.getComponent(null));
+    lines.add(Lang.COMMAND_HELP_TITLE.getComponent(null));
+    lines.add(Component.text(""));
+
+    for (SubCommand subCommand : sortedCommands.keySet()) {
+      if (sender.hasPermission(subCommand.getPermission())) {
+        final Component helpSubCommand = Lang.COMMAND_HELP_SUB_COMMAND.getComponent(new String[] { String.valueOf(number), subCommand.getSyntax() })
+          .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.SUGGEST_COMMAND, CoreUtil.getTextBeforeCharacter(subCommand.getSyntax(), '&')))
+          .hoverEvent(Lang.COMMAND_HELP_SUB_COMMAND_HOVER.getComponent(new String[] { subCommand.getDescription() }));
+        lines.add(helpSubCommand);
+        number++;
+      }
+    }
+
+    lines.add(Component.text(""));
+    lines.add(Lang.COMMAND_HELP_DIVIDER.getComponent(null));
+    for (Component line : lines) sender.sendMessage(line);
   }
 
   @Override
