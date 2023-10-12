936cb85f92d6262a69d9700aef8f1f5d356043dc
Lee <leecook808@gmail.com>
2023-10-12 13:21:20 -0700

    fix

--- src/main/java/lee/code/villagers/listeners/InteractListener.java
+++ src/main/java/lee/code/villagers/listeners/InteractListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class InteractListener implements Listener {
@@ -17,15 +18,21 @@ public class InteractListener implements Listener {
   }
 
   @EventHandler
+  public void onInteractAtVillager(PlayerInteractAtEntityEvent e) {
+    if (!(e.getRightClicked() instanceof Villager)) return;
+    final int id = villagers.getVillagerManager().getVillagerID(e.getRightClicked());
+    if (id == 0) return;
+    e.setCancelled(true);
+  }
+
+  @EventHandler
   public void onInteractWithVillager(PlayerInteractEntityEvent e) {
-    if (e.getRightClicked() instanceof Villager) {
-      //TODO add click delay
-      final int id = villagers.getVillagerManager().getVillagerID(e.getRightClicked());
-      if (id == 0) return;
-      e.setCancelled(true);
-      final Player player = e.getPlayer();
-      player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 1, 1);
-      villagers.getVillagerManager().runVillagerCommand(player, id);
-    }
+    if (!(e.getRightClicked() instanceof Villager)) return;
+    final int id = villagers.getVillagerManager().getVillagerID(e.getRightClicked());
+    if (id == 0) return;
+    e.setCancelled(true);
+    final Player player = e.getPlayer();
+    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 1, 1);
+    villagers.getVillagerManager().runVillagerCommand(player, id);
   }
 }
